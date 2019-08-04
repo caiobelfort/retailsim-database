de49c9afd42d56c46e457b48d0cf87bf484fd3fa
Caio Belfort <caiobelfort90@gmail.com>
2019-08-04 00:39:52 -0300

    Change to dump_data.sh to use port

--- dump_data.sh
+++ dump_data.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 HOST=$1
-DATABASE=$2
-USER=$3
-PASSWORD=$4
+PORT=$2
+DATABASE=$3
+USER=$4
+PASSWORD=$5
 
 export PGPASSWORD=$PASSWORD
 
 # Copy aisle
 echo Copying aisle data to products.aisle
-psql -h $HOST -d $DATABASE -U $USER -c \ "copy products.aisle (id, name) from STDIN with delimiter as ',' CSV HEADER;" < data/aisles.csv
+psql -h $HOST -p $PORT -d  $DATABASE -U $USER -c \ "copy products.aisle (id, name) from STDIN with delimiter as ',' CSV HEADER;" < data/aisles.csv
 
 # Copy department
 echo Copying department data to products.depatment
-psql -h $HOST -d $DATABASE -U $USER -c \ "copy products.department (id, name) from STDIN with delimiter as ',' CSV HEADER;" < data/departments.csv
+psql -h $HOST -p $PORT -d $DATABASE -U $USER -c \ "copy products.department (id, name) from STDIN with delimiter as ',' CSV HEADER;" < data/departments.csv
 
 # Copy products
 echo Copying product data to products.product
-psql -h $HOST -d $DATABASE -U $USER -c \ "copy products.product (id, name, aisle_id, department_id) from STDIN with delimiter as ',' CSV HEADER;" < data/products.csv
+psql -h $HOST -p $PORT -d $DATABASE -U $USER -c \ "copy products.product (id, name, aisle_id, department_id) from STDIN with delimiter as ',' CSV HEADER;" < data/products.csv
 
 
